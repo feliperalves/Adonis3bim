bd6bfdd0947379749dc1abd905ef6020b8755148
feliperalves <feliperchalves@gmail.com>
2022-10-21 09:54:56 -0300

    Tudo funcionando - 21/10/2022

--- database/migrations/1662113844728_users.ts
+++ database/migrations/1662113844728_users.ts
@@ -14,8 +14,8 @@ export default class UsersSchema extends BaseSchema {
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.datetime('created_at', { useTz: true }).notNullable()
-      table.datetime('updated_at', { useTz: true }).notNullable()
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
