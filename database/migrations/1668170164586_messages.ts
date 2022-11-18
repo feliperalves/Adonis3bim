b3de5e1b790e42f819be1bfe766fa66cc884f865
feliperalves <feliperchalves@gmail.com>
2022-11-17 22:16:09 -0300

    Feito

--- database/migrations/1668170164586_messages.ts
+++ database/migrations/1668170164586_messages.ts
@@ -17,8 +17,8 @@ export default class extends BaseSchema {
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.datetime('created_at', { useTz: true })
+      table.datetime('updated_at', { useTz: true })
     })
   }
 
